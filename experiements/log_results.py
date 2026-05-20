be24de35c279bf0c7fdd1495bbb8aafceb3e5908
likhitha050 <likhitha1ms24mc050@gmail.com>
2026-05-20 18:24:31 +0530

    Update log_results.py

--- experiements/log_results.py
+++ experiements/log_results.py
@@ -1,16 +1,20 @@
 import csv
 import os
+import time
 
 CSV_FILE = "results.csv"
 
 def log_to_csv(algo_name, operation, file_size_mb, exec_time, cpu_percent, energy):
+
     file_exists = os.path.isfile(CSV_FILE)
 
     with open(CSV_FILE, mode="a", newline="") as file:
         writer = csv.writer(file)
 
+        # Write header only once
         if not file_exists:
             writer.writerow([
+                "Timestamp",
                 "Algorithm",
                 "Operation",
                 "File Size (MB)",
@@ -20,10 +24,11 @@ def log_to_csv(algo_name, operation, file_size_mb, exec_time, cpu_percent, energ
             ])
 
         writer.writerow([
+            time.strftime("%Y-%m-%d %H:%M:%S"),
             algo_name,
             operation,
-            file_size_mb,
-            exec_time,
-            cpu_percent,
-            energy
+            round(float(file_size_mb), 6) if file_size_mb != "N/A" else "N/A",
+            round(exec_time, 6),
+            round(cpu_percent, 2),
+            round(energy, 6)
         ])
